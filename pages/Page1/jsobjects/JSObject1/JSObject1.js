dcba328d35f81d5de64a7120e54810b89962cc06
felipe <bac.smart.idea@gmail.com>
2024-05-14 21:01:30 +0000

    excluir page1

--- pages/Page1/jsobjects/JSObject1/JSObject1.js
+++
@@ -1,7 +0,0 @@
-export default {
-	async calcularData() {
-		let inicio = new Date(DatePicker1.selectedDate);
-		inicio.setDate(inicio.getDate()+ Input1.text)
-		Text1.setText(inicio.toLocaleDateString())
-	}
-}
